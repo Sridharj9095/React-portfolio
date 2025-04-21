c7e5aa5e9e5ca098a96cd536f63626b844e20d18
Sridhar Jeganathan <happybeingdarkevil@gmail.com>
2025-04-21 17:26:33 +0530

    added menu options by using lucide-react library.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,39 +1,42 @@
+import { useState } from "react";
+import { Menu, X } from "lucide-react";
+
 function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <header className="sticky top-0 z-50 bg-white shadow">
-      <nav className="flex justify-between items-center max-w-6xl mx-auto p-4">
+      <nav className="relative flex justify-between items-center max-w-6xl mx-auto p-4">
         <a
           href="/"
           className="text-2xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-black to-black hover:from-pink-500 hover:to-yellow-500 transition-all duration-300"
         >
           Sridhar Jeganathan
         </a>
-
-        <ul className="hidden md:flex gap-6">
-          <li>
-            <a
-              href="#portfolio"
-              className="hover:text-emerald-600 active:text-emerald-600 transition-colors duration-300"
-            >
-              Portfolio
-            </a>
-          </li>
-          <li>
-            <a
-              href="#projects"
-              className="hover:text-emerald-600 active:text-emerald-600 transition-colors duration-300"
-            >
-              Projects
-            </a>
-          </li>
-          <li>
-            <a
-              href="#contact"
-              className="hover:text-emerald-600 active:text-emerald-600 transition-colors duration-300"
-            >
-              Contact
-            </a>
-          </li>
+        <button
+          onClick={() => setIsOpen(!isOpen)}
+          className="md:hidden focus:outline-none z-50"
+        >
+          {isOpen ? <X size={28} /> : <Menu size={28} />}
+        </button>
+        <ul
+          className={`md:flex md:gap-6 md:static absolute top-full left-0 w-full bg-white px-6 md:px-0 py-4 md:py-0 shadow-md md:shadow-none transition-all duration-300 ease-in-out transform md:translate-y-0 z-40 ${
+            isOpen
+              ? "translate-y-0 opacity-100 visible"
+              : "-translate-y-5 opacity-0 invisible"
+          }`}
+        >
+          {["portfolio", "projects", "contact"].map((item) => (
+            <li key={item}>
+              <a
+                href={`#${item}`}
+                className="block py-2 md:py-0 hover:text-emerald-600 active:text-emerald-600 transition-colors duration-300"
+                onClick={() => setIsOpen(false)}
+              >
+                {item.charAt(0).toUpperCase() + item.slice(1)}
+              </a>
+            </li>
+          ))}
         </ul>
       </nav>
     </header>
